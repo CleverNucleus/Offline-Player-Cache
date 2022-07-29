93deae6dc326735ee87a9c086ce71812590f8939
CleverNucleus <jamie@mclean007.plus.com>
2022-07-29 02:03:27 +0100

    Fixed potential issue
    *If for some reason a player's name had changed since they were last logged on, their cache would not be cleared upon login. When disconnecting, server would subsequently crash due to a different name being used as a key trying to put an already existing value into a Bimap.

--- src/main/java/com/github/clevernucleus/opc/impl/OfflinePlayerCacheImpl.java
+++ src/main/java/com/github/clevernucleus/opc/impl/OfflinePlayerCacheImpl.java
@@ -122,7 +122,7 @@ public final class OfflinePlayerCacheImpl {
 			Map<CacheableValue<?>, Object> value = new HashMap<>();
 			KEYS.forEach((identifier, key) -> value.put(key, key.get(player)));
 			this.cache.put(uuid, value);
-			this.nameToId.put(name, uuid);
+			this.nameToId.inverse().put(uuid, name);
 			return true;
 		});
 	}
@@ -130,7 +130,7 @@ public final class OfflinePlayerCacheImpl {
 	public boolean uncache(final ServerPlayerEntity player) {
 		return this.isValid(player, (uuid, name) -> {
 			this.cache.remove(uuid);
-			this.nameToId.remove(name);
+			this.nameToId.inverse().remove(uuid);
 			return true;
 		});
 	}
